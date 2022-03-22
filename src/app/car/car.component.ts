8d1e1d2d1c5f8182d8b1f8e8e03256be1ba25ef7
henry-jessica <s00214115@mail.itsligo.ie>
2022-03-22 22:34:04 +0000

    create car component

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ICar } from '../interfaces/car';
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+  @Input() carData!: ICar; 
+  carImageWidth: number = 300; 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
