d8dfa54ff13845ca36186416c58089e6cea52eae
henry-jessica <s00214115@mail.itsligo.ie>
2022-03-22 22:35:35 +0000

    add firebase compat on module

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from "../environments/environment";
+import { CarlistComponent } from './carlist/carlist.component';
+import { CarComponent } from './car/car.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarlistComponent,
+    CarComponent
+  ],
+  imports: [
+    BrowserModule, 
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
