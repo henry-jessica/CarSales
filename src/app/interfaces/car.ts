c13c5dd5756dd502d5535d08f5330ab419109ae1
henry-jessica <s00214115@mail.itsligo.ie>
2022-03-22 22:34:32 +0000

    create car interface

---
+++ src/app/interfaces/car.ts
@@ -0,0 +1,20 @@
+export interface ICar {
+    make: string; 
+    model: string; 
+    year: string; 
+    imageURL: string;
+}
+
+export class Car{
+    make: string; 
+    model: string; 
+    year: string; 
+    imageURL: string; 
+
+    constructor(make: string, model: string, year: string, imageURL: string) {
+        this.make = make; 
+        this.model = model; 
+        this.year = year; 
+        this.imageURL = imageURL; 
+    }
+}
