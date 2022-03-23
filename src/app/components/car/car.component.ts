4501b533fd3542643b246637c28048d05853fa46
henry-jessica <s00214115@mail.itsligo.ie>
2022-03-23 03:29:19 +0000

    add delete car functionality

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,16 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ICar } from '../../interfaces/car';
+import { ICar} from '../../interfaces/car';
+import { CarApiService } from '../../service/car-api-service';
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
+
 })
 export class CarComponent implements OnInit {
-  @Input() carData!: ICar; 
+  @Input() carData!: any; 
   carImageWidth: number = 300; 
-  constructor() { }
-
+  carsData!: ICar[];
+  constructor(private _carAPIService: CarApiService) { }
+  
   ngOnInit(): void {
   }
-
+  deleteCar() {
+    console.log(this.carData);
+    this._carAPIService.delCarData(this.carData.id); 
+    return false;
+    }
 }
--- src/app/service/car-api-service.ts
+++ src/app/service/car-api-service.ts
@@ -8,8 +8,8 @@ import { ICar } from '../interfaces/car';
 @Injectable({
   providedIn: 'root'
 })
-export class CarApiService {
-
+  
+export class CarApiService {  
   //service wrapper around the native firestore SDK's 
   //CollectionReference and Query types.
   carsDataCollection!: AngularFirestoreCollection<ICar>; 
@@ -19,6 +19,7 @@ export class CarApiService {
 
   //array to hold all cars
   allCarsData!: ICar[]; 
+  
 
   errorMessage!: string; 
   constructor(private _http: HttpClient, private _afs: AngularFirestore) {
@@ -26,17 +27,25 @@ export class CarApiService {
     this.carsDataCollection = _afs.collection<ICar>("cars_data"); 
   }
   getCarData(): Observable<ICar[]>{
-    this.carsData = this.carsDataCollection.valueChanges(); 
+    this.carsData = this.carsDataCollection.valueChanges({idField:'id'}); 
     this.carsData.subscribe(
       data=>console.log("getCarsData"+JSON.stringify(data))
     )
     return this.carsData; 
   }
-  addCarData(car: ICar): void{
+
+ addCarData(car: ICar): void{
     this.carsDataCollection.add(JSON.parse(JSON.stringify(car))); 
-  }
+      }
+  
+ delCarData(carId: any): void{
+    this.carsDataCollection.doc(carId).delete(); 
+}
+  
   private handleError(err: HttpErrorResponse) {
     console.log('CarApiService' + err.message); 
     return Observable.create(err.message); 
   }
 }
+
+
